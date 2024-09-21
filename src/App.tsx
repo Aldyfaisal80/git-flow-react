2be19dc5fffa3a538bc68ae7098c01ffacbbbfa4
aldyfaisal80 <aldyfaisal80@gmail.com>
2024-09-21 09:44:37 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="container">
+            <h1 className="title">Avnexcode</h1>
+            <h1 className="title"><span>Prot Prot</span> Builder</h1>
+            <h1 className="title">Next JS App Router Builder</h1>
+        </div>
+    );
+}
